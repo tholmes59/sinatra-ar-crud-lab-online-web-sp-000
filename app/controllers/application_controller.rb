9abd5da755d045c6e1829d66e10e7b10d855da61
Tom Holmes <tholmes59@gmail.com>
2019-04-01 17:37:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,44 +12,37 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles/new"
   end
 
-  # index
   get "/articles" do
     @articles = Article.all
     erb :index
   end
 
-  # new
   get "/articles/new" do
     @article = Article.new
     erb :new
   end
 
-  # create
   post "/articles" do
     @article = Article.create(params)
     redirect to "/articles/#{ @article.id }"
   end
 
-  # show
   get "/articles/:id" do
     @article = Article.find(params[:id])
     erb :show
   end
 
-  # edit
   get "/articles/:id/edit" do
     @article = Article.find(params[:id])
     erb :edit
   end
 
-  # update
   patch "/articles/:id" do
     @article = Article.find(params[:id])
     @article.update(params[:article])
     redirect to "/articles/#{ @article.id }"
   end
 
-  #destroy
   delete "/articles/:id" do
     Article.destroy(params[:id])
     redirect to "/articles"
